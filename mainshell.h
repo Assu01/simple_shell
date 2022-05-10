c06170638b9ea6e0a16d4273a20712bce0362f23
Minte123 <netacademy1986minte@yahoo.com>
2022-05-10 12:04:06 +0300

    Header file

---
+++ mainshell.h
@@ -0,0 +1,36 @@
+#ifndef _MINISHELL_H_
+#define _MINISHELL_H_
+
+#include <stddef.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "typedefs.h"
+#include "constants.h"
+
+
+/* global environemnt */
+extern char **environ;
+/* global program name */
+char *name;
+/* global aliases linked list */
+alias_t *aliases;
+
+/* signals and processes */
+void signal_handler(int);
+
+/* helpers for user inputs */
+char *get_user_input();
+
+/* helpers errors handling */
+/* 0 : readline_error */
+void readline_error(void);
+
+void (*handle_error[])(void) = {
+    readline_error
+};
+
+#endif
