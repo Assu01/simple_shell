9932591a775f1d72f96167f07e8fb0f1ac76b747
Minte123 <netacademy1986minte@yahoo.com>
2022-05-18 19:31:20 +0300

    Super Simple Shell

--- shell.c
+++ shell.c
@@ -1,57 +1,113 @@
-#include "shell.h"
+ #include "shell.h"
+
+int _strlen(char *str)
+{
+	int len;
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	return (len + 1);
+}
+
+
+int count_args(char *user_input)
+{
+	int args, i;
+
+	args = 1;
+	i = 0;
+	while (user_input[i] != '\0' && user_input[i] != '\n')
+	{
+		if (user_input[i] == ' ')
+			args++;
+		i++;
+	}
+	return (args);
+}
+
+
+void get_command(char *user_input, int args)
+{
+	int compare, status, i, length;
+	pid_t child_pid;
+	char *token;
+	char **argv;
+
+	compare = strncmp("exit", user_input, 4);
+	if (compare == 0)
+	{
+		free(user_input);
+		exit (0);
+	}
+
+	length = 0;
+	argv = malloc(sizeof(char *) * (args + 1));
+
+	status = 1;
+
+	token = strtok(user_input, " \n");
+	length = _strlen(token);
+	argv[0] = malloc(sizeof(char) * length);
+	argv[0] = token;
+	token = strtok(NULL, " \n");
+	for (i = 1; token != NULL; i++)
+	{
+		length = _strlen(token);
+		argv[i] = malloc(sizeof(char) * length);
+		argv[i] = token;
+		token = strtok(NULL, " \n");
+	}
+
+	argv[i] = '\0';
+
+	child_pid = fork();
+		if (child_pid == 0)
+		{
+			if (execvp(argv[0], argv) != 0)
+				perror("./hsh");
+
+			for (i = 0; i < args; i++)
+				free(argv[i]);
+			free(argv);
+		}
+		else
+			wait(&status);
+
+}
 
 /**
- * main - a simple shell program written in C
- * @argc: number of arguments
- * @argv: array of arguments
- * @env: array of environment variables
- *
- * Return: 0 always (but program may exit early)
+ * main - simple shell
+ * Return: 0 (Always)
  */
 
-int main(__attribute__((unused)) int argc, char **argv, char **env)
+int main(void)
 {
+	int bytes_read, args;
 	char *user_input;
 	size_t nbytes;
-	ssize_t bytes_read;
-	char **commands, **path_array;
-	static char *NAME;
 
-	NAME = argv[0];
-	user_input = NULL;
-	nbytes = 0;
-	commands = NULL;
-	path_array = NULL;
-	while (1)
+	nbytes = 1024;
+	while (nbytes != 0)
 	{
 		write(STDOUT_FILENO, "$ ", 2);
+		user_input = malloc(sizeof(char) * nbytes);
+		if (user_input == NULL)
+		{
+			perror("malloc\n");
+			break;
+		}
 		bytes_read = getline(&user_input, &nbytes, stdin);
 		if (bytes_read == -1)
 		{
-			perror(NAME);
+			perror("getline\n");
 			break;
 		}
-
-		if (bytes_read == 5)
-			exit_check(user_input);
-
-		if (blank_check(user_input) == 1)
-			continue;
-
-		if (env_check(user_input) == 1)
-		{
-			print_env(env);
-			continue;
-		}
-
-		path_array = get_path_array(env);
-		commands = parse_input(user_input, path_array, NAME);
-		fork_wait_exec(commands, env);
-		free_commands(commands);
-
-		/*free(path_array);*/
-		/*path_array = NULL;*/
-		/*free(user_input);*/
+		args = count_args(user_input);
+		get_command(user_input, args);
 	}
+	free(user_input);
 	return (0);
 }
--- shell.h
+++ shell.h
@@ -1,37 +1,9 @@
 #ifndef SHELL_H
 #define SHELL_H
-
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-
-/* check_helpers */
-void exit_check(char *user_input);
-int blank_check(char *user_input);
-int path_check(char *command);
-int env_check(char *user_input);
-
-/* fork_wait_exec */
-void fork_wait_exec(char **commands, char **env);
-
-/* memory_helpers */
-void free_commands(char **commands);
-
-/* parse_input */
-char **parse_input(char *user_input, char **path_array, char *NAME);
-
-/* string_helpers */
-int _strlen(char *str);
-char *_strcat(char *dest, char *src);
-char *_strdup(char *str);
-
-/* env_helpers */
-int get_path_count(char *path);
-char **get_path_array(char **env);
-char *find_path(char **path_array, char *token);
-void print_env(char **env);
-
 #endif
